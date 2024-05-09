9404ef1dd3be62cd8de5253444f0195ba81919d7
UHSER <sergiodejesusuhmarquez@gmail.com>
2024-05-08 23:24:55 -0500

    Creating Question class

---
+++ question.py
@@ -0,0 +1,8 @@
+"""Question class."""
+class Question:
+    """Constructor (Initializing 2 attributes "text" and "answer")"""
+    def __init__(self,q_text, q_answer):
+        self.text = q_text
+        self.answer =q_answer
+#q = Question("23",False)
+#print(q.answer)
