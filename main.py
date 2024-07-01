905c9979a96e3ffb6f43ba5c4d2e28dc78ac97e8
UHSER <sergiodejesusuhmarquez@gmail.com>
2024-06-30 23:33:48 -0500

    Json data

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 """Imports"""
+import pathlib
+import json
 from question import Question
-from data import question_data
 from brain import QuizBrain
 
+RUTA = pathlib.Path(__file__).parent
+with open(f"{RUTA}/data.json",mode="r",encoding="UTF8") as jdata:
+    data = json.load(jdata)
+
+question_data = data["true_false"]
+
 question_bank = []
+
 for question in question_data:
-    QT = question["question"]
-    QA = question["correct_answer"]
-    new_question = Question(QT,QA)
+    q_text = question["question"]
+    q_answer = question["correct_answer"]
+    new_question = Question(q_text,q_answer)
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
